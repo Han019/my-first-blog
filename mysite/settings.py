d84e2863983a9d529d3b7b2d663a84494f266614
Han019 <gkstmdgjs1119@gmail.com>
2025-10-13 14:53:18 +0900

    ssss

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog",
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,14 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+MEDIA_URL= "/media/"
+MEDIA_ROOT=BASE_DIR/"media/"
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES':[
+        #'rest_framework.permissions.IsAdminUser',
+    ],
+    'PAGE_SIZE':10
+}
---
+++ test_api/test_api.py
@@ -0,0 +1,40 @@
+import requests
+from PIL import Image
+import pillow_heif
+import os
+
+direc='./mountain.HEIC'
+pillow_heif.register_heif_opener()
+img=Image.open(direc)
+img.save('./mountain.jpg',format("jpeg"))
+
+
+HOST="http://127.0.0.1:8000"
+res= requests.post(HOST +"/api-token-auth/",{
+    'username':'admin',
+    'password':'1q2w3e4r', 
+})
+
+res.raise_for_status()
+token= res.json()['token']
+print(token)
+
+headers={
+    'Authorization':'JWT' + token,
+    'Accept':'application/json'}
+
+#Post create
+
+data={
+    'author':"1",
+    'title':'제목 by code',
+    'text': 'API 내용 by code',
+    'created_date': '2025-05-10T18:34:00+09:00',
+    'published_date': '2025-05-10T18:34:00+09:00',
+}
+#경로
+
+file={'image': open('./mountain.jpg','rb')}
+res=requests.post(HOST +'/api_root/Post/',data=data,files=file, headers= headers)
+print(res)
+print(res.json())
